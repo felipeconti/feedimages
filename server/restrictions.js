6caba8f2854e9532760e7d0692d6777125a88b31
Felipe Conti <felipeconti18@gmail.com>
2015-10-14 00:11:15 -0300

    Added descriptions and restrictions

--- server/restrictions.js
+++ server/restrictions.js
@@ -3,6 +3,12 @@ Images.allow({
 		// add custom authentication code here
 		return true;
 	},
+	update: function(){
+		return true;
+	},
+	remove: function(){
+		return true;
+	},
 	download: function() {
 		return true;
 	}
